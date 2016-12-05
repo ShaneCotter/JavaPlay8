aacb767996f37d9c55a8d64fe4d991b36b892104
X00131736 <X00131736@computing.stu.it-tallaght.ie>
2016-12-05 14:45:58 +0000

    finished

---
+++ app/controllers/HomeController.java
@@ -0,0 +1,38 @@
+package controllers;
+
+import play.api.Environment;
+import play.mvc.*;
+import play.data.*;
+import play.db.ebean.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+
+import views.html.*;
+
+//Import models
+import models.*;
+
+public class HomeController extends Controller {
+
+    public Result index(String name) {
+        return ok(index.render("Welcome to the Home page", name));
+    }
+
+    public Result about() {
+        return ok(about.render());
+    }
+
+    public Result products() {
+
+        //Get the list of products
+        List<Product> productsList = Product.findAll();
+
+        //Render the list products view, passing parameters
+        return ok(products.render(productsList));
+    }
+
+}
+
+
